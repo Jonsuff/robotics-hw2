756f0b17f3a105a030ab67a7ea9e7badaaca6302
Jonsuff <dbwhdtjq92@gmail.com>
2019-11-04 22:58:04 +0900

    service added

--- sensor/src/message_publisher.py
+++ sensor/src/message_publisher.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import rospy
 from common_msgs.msg import SelfMadeMessage
+from common_msgs.srv import SelfMadeService, SelfMadeServiceRequest
+
 
 rospy.init_node('message_publisher', anonymous=True)
-
-
+requester = rospy.ServiceProxy('reset', SelfMadeService)
+count = 0
 msg = SelfMadeMessage()
 
 pub = rospy.Publisher("DateTime_msg", SelfMadeMessage, queue_size = 1)
@@ -19,6 +21,7 @@ msg.rotation.position.y = 0.0
 msg.rotation.position.z = 0.0
 
 while not rospy.is_shutdown():
+    count += 1
     msg.number.data += 1
     msg.rotation.orientation.x += 1
     msg.rotation.orientation.y += 0.01
@@ -28,6 +31,12 @@ while not rospy.is_shutdown():
     msg.rotation.position.y += 2
     msg.rotation.position.z += 3
     pub.publish(msg)
+    if count == 10:
+        req = SelfMadeServiceRequest(type1 = True, type2 = False)
+        res = requester(req)
+        count = 0
+    
+    
     print(msg.number)
     print(msg.rotation)
     rate.sleep()
